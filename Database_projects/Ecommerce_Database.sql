e1822756ae911666e1def0270dcaee685a990969
Hiwotarayaa <48228547+Hiwotarayaa@users.noreply.github.com>
2025-03-05 19:07:06 -0500

    Create Ecommerce_Database.sql

---
+++ Database_projects/Ecommerce_Database.sql
@@ -0,0 +1,20 @@
+
+
+****#Add SQL Schema for a sample project 
+***Schema Example:
+
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    signup_date DATE
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10,2),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
